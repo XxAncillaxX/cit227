07e687940e6584e96f3bbd924d6f9b1876b0ee15
Maddie Hartley <mfhartley@mail.wvncc.edu>
2023-04-08 10:11:19 -0400

    Updated new main

--- Game/main.lua
+++ Game/main.lua
@@ -1,2 +1,24 @@
-function love.load()
+--Require Section
+Input = require('library/Input')
 
+
+function love.load()
+    input = Input()
+
+    hero = {
+        x = 0,
+        y = 0,
+        w = 0,
+        h = 0,
+        image = love.graphics.newImage("assets/graphics/hero.png")
+    }
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+    love.graphics.draw(hero.image, hero.x, hero.y, 0, 3, 3)
+end
