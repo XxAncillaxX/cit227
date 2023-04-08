c7f62e94c03de5964f43582da08b3b1fcfd7ddae
Maddie Hartley <mfhartley@mail.wvncc.edu>
2023-04-08 09:49:41 -0400

    Corrected Input and added lua main code

--- Game/main.lua
+++ Game/main.lua
@@ -1,6 +1,23 @@
 
+Input = require("library/Input")
+
 function love.load()
+    input = Input()
+    input:bind('d', 'right')
+    input:bind('a', 'left')
+    input:bind('space', 'action')
+    --input:bind('')
     love.graphics.setBackgroundColor(0, 0, 0, 0)
+end
+
+function handleInput(dt)
+    if input:down('right') then
+        rect1.speed = rect1.topSpeed
+    elseif input:down('left') then
+        rect1.speed = -rect1.topSpeed
+    else
+        rect1.speed = 0
+    end
 end
 
 function love.update(dt)
