bc610434a774b136cc28747a68df5d5eb9fd6a51
Maddie Hartley <mfhartley@mail.wvncc.edu>
2023-04-08 22:58:20 -0400

    saved tilled map files

--- Game/assets/maps/collectibles.tsx
+++ Game/assets/maps/collectibles.tsx
@@ -1,4 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.1" name="collectibles" tilewidth="16" tileheight="16" tilecount="8" columns="4">
  <image source="../graphics/spritesheets/power-up.png" width="64" height="32"/>
+ <tile id="0">
+  <properties>
+   <property name="collectible" value="machineGun"/>
+   <property name="points" type="int" value="500"/>
+  </properties>
+ </tile>
 </tileset>
